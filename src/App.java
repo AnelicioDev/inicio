16f55908984058b11b5fb84c8925af393b7651b6
AnelicioDev <anelicio.dev@gmail.com>
2024-04-23 23:58:52 -0300

    commit intellij teste

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //teste
-        System.out.println("Hello, World!");
+       int num1 = 10, num2 = 5;
+       int soma = num1 + num2;
+       System.out.println("Resultado: " + soma);
     }
 }
